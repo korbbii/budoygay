960582d57b91bfbc20e86fd6fdef00e7e8fc5427
korbbii <124796384+korbbii@users.noreply.github.com>
2023-11-13 08:44:12 +0800

    Add files via upload

---
+++ budoy/src/main/java/com/mycompany/budoy/Budoy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.budoy;
+
+/**
+ *
+ * @author user
+ */
+public class Budoy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
